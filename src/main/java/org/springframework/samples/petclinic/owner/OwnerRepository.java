9a0a646621beb673db734b20a9ba172538e3705c
sagarlahade0910 <sagar.lahade@configserverllp.com>
2026-04-27 12:49:42 +0530

    made more changes

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerRepository.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerRepository.java
@@ -25,14 +25,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 /**
- * Repository class for <code>Owner</code> domain objects All method names are compliant
+ * change class for <code>Owner</code> domain objects All method names are compliant
  * with Spring Data naming conventions so this interface can easily be extended for Spring
  * Data. See:
  * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.query-methods.query-creation
  *
- * @author Ken Krebs
- * @author Juergen Hoeller
- * @author Sam Brannen
+
  * @author Michael Isvy
  * @author Wick Dynex
  */
@@ -44,6 +42,8 @@ public interface OwnerRepository extends JpaRepository<Owner, Integer> {
 	 */
 	@Query("SELECT ptype FROM PetType ptype ORDER BY ptype.name")
 	List<PetType> findPetTypes();
+	List<PetType> findPetTypes();
+
 
 	/**
 	 * Retrieve {@link Owner}s from the data store by last name, returning all owners
