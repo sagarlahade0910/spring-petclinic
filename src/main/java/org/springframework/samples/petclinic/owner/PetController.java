fd02fd175d7c56198b59e8b2364a9bcf914fd903
sagarlahade0910 <sagar.lahade@configserverllp.com>
2026-04-27 15:40:54 +0530

    SecondRound PetController on seondround branch

--- src/main/java/org/springframework/samples/petclinic/owner/PetController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetController.java
@@ -42,7 +42,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
  */
 @Controller
 @RequestMapping("/owners/{ownerId}")
-class PetController {
+class SecondRoundPetController {
 
 	private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
 
